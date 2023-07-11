cf80ab13b006cb55586e280afa66e044f7a3ed25
Stacey Brogden <toad@brothertoad.net>
2023-07-10 19:14:52 -0700

    Initial commit.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/brothertoad/add-prefix
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+  "os"
+)
+
+func main() {
+  if len(os.Args) < 3 {
+    fmt.Printf("Need at least two arguments\n")
+    os.Exit(1)
+  }
+  prefix := os.Args[1]
+  fmt.Printf("prefix is %s\n", prefix)
+  files := os.Args[2:]
+  for _, file := range(files) {
+    target := prefix + file
+    fmt.Printf("Moving %s to %s...\n", file, target)
+    if err := os.Rename(file, target); err != nil {
+      fmt.Printf("Unable to rename %s to %s: %s\n", file, target, err.Error())
+    }
+  }
+}
